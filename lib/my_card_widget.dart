4af69e6d153726baf64def5591ce8a63446f5370
Pashko Dmytro <pashko.dmytro@gmail.com>
2022-08-27 23:43:32 +0300

    Implemented logic of opening Github and Linkeding web page after click on icons. Implemented logic to start mail client after click on email address.

--- lib/my_card_widget.dart
+++ lib/my_card_widget.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri linkedinUri = Uri.parse('https://www.linkedin.com/in/dmitry-pashko');
+final Uri githubUri = Uri.parse('https://github.com/Dmytro-Pashko');
+final Uri mailtoUri = Uri.parse('mailto:pashko.dmytro@gmail.com');
 
 class MyCardWidget extends StatelessWidget {
   const MyCardWidget({Key? key}) : super(key: key);
+
+  Future<void> openWebUrl(Uri url) async {
+    if (!await launchUrl(url)) {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,22 +26,22 @@ class MyCardWidget extends StatelessWidget {
             children: [
               const CircleAvatar(
                 radius: 75,
-                backgroundImage: AssetImage("images/profile_avatar.jpg"),
+                backgroundImage: AssetImage('images/profile_avatar.jpg'),
               ),
               const Text(
-                "Dmytro Pashko",
+                'Dmytro Pashko',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 30,
-                  fontFamily: "Pacifico",
+                  fontFamily: 'Pacifico',
                 ),
               ),
               Text(
-                "Mobile Tech Lead",
+                'Mobile Tech Lead',
                 style: TextStyle(
                   color: Colors.teal[50],
                   fontSize: 25,
-                  fontFamily: "Teko",
+                  fontFamily: 'Teko',
                 ),
               ),
               SizedBox(
@@ -51,10 +62,10 @@ class MyCardWidget extends StatelessWidget {
                       color: Colors.teal,
                     ),
                     title: Text(
-                      "+38(099)** *** **",
+                      '+38(099)** *** **',
                       style: TextStyle(
                           fontSize: 20,
-                          fontFamily: "Cario",
+                          fontFamily: 'Cario',
                           color: Colors.teal.shade900),
                     )),
               ),
@@ -62,19 +73,25 @@ class MyCardWidget extends StatelessWidget {
                 color: Colors.white,
                 margin:
                     const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: ListTile(
-                    leading: const Icon(
-                      Icons.email,
-                      size: 35,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      "pashko.dmytro@gmail.com",
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontFamily: "Cario",
-                          color: Colors.teal.shade900),
-                    )),
+                child: TextButton(
+                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                  onPressed: () {
+                    launchUrl(mailtoUri);
+                  },
+                  child: ListTile(
+                      leading: const Icon(
+                        Icons.email,
+                        size: 35,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'pashko.dmytro@gmail.com',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontFamily: 'Cario',
+                            color: Colors.teal.shade900),
+                      )),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 32),
@@ -84,14 +101,18 @@ class MyCardWidget extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       TextButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            openWebUrl(linkedinUri);
+                          },
                           child: SvgPicture.asset("images/linkedin_icon.svg")),
                       SizedBox(
                         width: 20,
                       ),
                       TextButton(
-                          onPressed: () {},
-                          child: SvgPicture.asset("images/github_icon.svg")),
+                          onPressed: () {
+                            openWebUrl(githubUri);
+                          },
+                          child: SvgPicture.asset('images/github_icon.svg')),
                     ],
                   ),
                 ),
